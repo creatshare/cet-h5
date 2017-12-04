68da437956795b57fae12babf30dec55ff23ef00
hylerrix <hylerrix@foxmail.com>
2017-12-05 03:04:57 +0800

    :sparkles: add html2canvas to print res page

--- src/js/_base.js
+++ src/js/_base.js
@@ -1,3 +1,14 @@
 function postInfo () {
     window.location.href = './result.html'
 }
+
+window.onload = function () {
+    var rScreen = document.getElementById('rScreen')
+    html2canvas(rScreen, {
+        allowTaint: true,
+        height: 1000
+    }).then(function(canvas) {
+        // 用图片替换 DOM 结点
+        rScreen.parentNode.replaceChild(canvas, rScreen)
+    });
+}
